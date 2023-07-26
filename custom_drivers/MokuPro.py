e076dde874c6bfc5beb61057a6881fa98e30fcff
ajsousal <a.j.sousadealmeida@utwente.nl>
2023-07-26 11:08:09 +0200

    moku pro driver

--- custom_drivers/MokuPro.py
+++ custom_drivers/MokuPro.py
@@ -15,6 +15,22 @@ class MokuScope(Instrument):
         super().__init__(name,**kwargs)
         
         self.i = Oscilloscope(IP_address, force_connect=True)
+
+
+
+        def get_reading_buffer():
+            path_to_save = 'C:\\_measurement_software\\qtNE\\tmp'
+            fname_to_save = 'tmp_data.npy'
+
+            response = self.i.save_high_res_buffer(comments='tmp')
+            fname_memory = response['file_name']
+            self.i.download(target='persist',file_name=fname_to_save,local_path=path_to_save)
+            self.i.delete(target='persist',file_name=fname_memory)
+            data_raw = np.load(os.path.join(path_to_save,fname_to_save))
+            # print(data_raw)
+            data_array = data_raw # [array(data_raw['time']), array(data_raw['ch'+dictionary['input_ch']])/dictionary['gain']]
+            
+            return data_raw
         
         
 
