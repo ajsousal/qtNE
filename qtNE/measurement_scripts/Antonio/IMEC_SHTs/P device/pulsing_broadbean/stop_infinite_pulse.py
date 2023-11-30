9c4d411d56bf820da74527cf13918cdf3ab46ca4
ajsousal <a.j.sousadealmeida@utwente.nl>
2023-11-30 15:19:11 +0100

    cleanup

--- qtNE/measurement_scripts/Antonio/IMEC_SHTs/P device/pulsing_broadbean/stop_infinite_pulse.py
+++
@@ -1,8 +0,0 @@
-station.awg.stop()
-station.awg.ch1_state(0)
-station.awg.ch2_state(0)
-station.awg.ch3_state(0)
-station.awg.ch4_state(0)
-station.psg.off()
-
-print('Pulse stopped and Chs 1, 2, 3 OFF')
